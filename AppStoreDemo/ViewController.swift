8cdc5c64d85d794525c540022548ca56c1333c93
Andrei Volkau <101.andrei.wolf@mail.ru>
2020-08-19 13:39:52 +0300

    Initial Commit

---
+++ AppStoreDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreDemo
+//
+//  Created by Andrei Volkau on 19.08.2020.
+//  Copyright © 2020 Andrei Volkau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
